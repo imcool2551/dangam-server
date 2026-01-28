c5135abe1673bd60c827752fbac67af847bdceef
sanguk <ocean@tesser.io>
2026-01-29 00:38:19 +0900

    fix: cursor seperator

--- src/util.mongo.ts
+++ src/util.mongo.ts
@@ -1,27 +1,34 @@
 import mongoose from 'mongoose';
 import { last } from 'lodash';
 
+const CURSOR_SEPARATOR = '::';
+
 export const nextCursorOf = (items: any[], ...fields: string[]) => {
   const item = last(items)
   if (!item) return undefined
 
-  return fields.map((key) => item[key] ?? '').join('_')
+  return fields.map((key) => item[key] ?? '').join(CURSOR_SEPARATOR)
 }
 
 export const parseCursorOf = (field: string, next?: string, direction: number = -1) => {
   if (!next) return undefined
 
-  const composite = next.split('_', 2)
+  const separatorIndex = next.lastIndexOf(CURSOR_SEPARATOR)
+  if (separatorIndex === -1) return undefined
+
+  const id = next.substring(0, separatorIndex)
+  const timestamp = Number(next.substring(separatorIndex + CURSOR_SEPARATOR.length))
+
+  if (isNaN(timestamp)) return undefined
+
   const condition = direction < 0 ? '$lt' : '$gt'
-  return composite?.length === 2
-    ? {
-      $or: [
-        { [field]: { [condition]: Number(composite[1]) } },
-        {
-          [field]: Number(composite[1]),
-          _id: { [condition]: composite[0] }
-        }
-      ]
-    }
-    : {}
+  return {
+    $or: [
+      { [field]: { [condition]: timestamp } },
+      {
+        [field]: timestamp,
+        _id: { [condition]: id }
+      }
+    ]
+  }
 }
